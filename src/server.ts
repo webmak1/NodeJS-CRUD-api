65773d6501b93700f00cd70d4994bd15c936c052
Kate Shlykova <e2.shlykova@itransition.com>
2022-06-19 21:59:03 +0300

    feat: add cross-env

--- src/server.ts
+++ src/server.ts
@@ -64,14 +64,13 @@ const updateUser = async (id:string, data:string) => {
 
 const deleteUser = async (id:string) => {
     users = users.filter((user:IUser) => user.id !== id);
-
 }
 
 export const server = createServer(async (req, res) => {
     if (req.url) {
         switch (req.method) {
             case 'POST': {
-                if (req.url === '/api/users') {
+                if (req.url === '/api/users' || req.url === '/api/users/') {
                     req.on('data', async (data) => {
                         const userProperties = JSON.parse(data.toString());
                         let message = '';
@@ -93,15 +92,18 @@ export const server = createServer(async (req, res) => {
                             res.end(JSON.stringify({message: `Enter the ${message}field(s)`}));
                         }
                     });
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({message: 'This is not correct url'}));
                 }
                 break;
             }
             case 'GET': {
-                if (req.url === '/api/users') {
+                if (req.url === '/api/users' || req.url === '/api/users/') {
                     const users = await getUsers();
                     res.writeHead(200, { 'Content-Type': 'application/json', 'Process-id': pid});
                     res.end(JSON.stringify(users));
-                } else if (req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
+                } else if (req.url.startsWith('/api/users/') && req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
                     const id = req.url.split('/')[3];
                     const user = await getUser(id);
                     if (user) {
@@ -111,14 +113,17 @@ export const server = createServer(async (req, res) => {
                         res.writeHead(404, { 'Content-Type': 'application/json' });
                         res.end(JSON.stringify({message: "User not found"}));
                     }
-                } else {
+                } else if (req.url.startsWith('/api/users/') && !req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)){
                     res.writeHead(400, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({message: "Users id is invalid"}));
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({message: 'This is not correct url'}));
                 }
                 break;
             }
             case 'PUT': {
-                if (req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
+                if (req.url.startsWith('/api/users/') && req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
                     const id = req.url.split('/')[3];
 
                     req.on('data', async (data) => {
@@ -147,14 +152,17 @@ export const server = createServer(async (req, res) => {
                             res.end(JSON.stringify({message: `Enter the ${message}field(s)`}));
                         }
                     });
-                } else {
+                } else if (req.url.startsWith('/api/users/') && !req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
                     res.writeHead(400, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({message: "Users id is invalid"}));
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({message: 'This is not correct url'}));
                 }
                 break;
             }
             case 'DELETE': {
-                if (req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
+                if (req.url.startsWith('/api/users/') && req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
                     const id = req.url.split('/')[3];
                     const user = users.find((user:IUser) => user.id === id);
 
@@ -166,11 +174,18 @@ export const server = createServer(async (req, res) => {
                         res.writeHead(404, { 'Content-Type': 'application/json' });
                         res.end(JSON.stringify({message: "User not found"}));
                     }
-                } else {
+                } else if (req.url.startsWith('/api/users/') && !req.url.match(/\/api\/users\/[\da-z]{8}-[\da-z]{4}-[\da-z]{4}-[\da-z]{4}-[\da-z]{12}/)) {
                     res.writeHead(400, { 'Content-Type': 'application/json' });
                     res.end(JSON.stringify({message: "Users id is invalid"}));
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({message: 'This is not correct url'}));
                 }
                 break;
+            }
+            default: {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({message: 'Use only GET, POST, PUT or DELETE method'}));
             }
         }
     }
